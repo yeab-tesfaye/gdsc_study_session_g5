b6b50d6837db1fc23d52300c50d874ded01305b8
yeab-tesfaye <150955560+yeab-tesfaye@users.noreply.github.com>
2023-11-25 19:16:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// function oddNum(num){
+//     for(i=0;i<=num;i++){
+//         if(i%2 != 0){
+//         console.log(i)
+//         }
+//     }
+// }
+// oddNum(7);
+
+function BMI(w,h){
+    return w/(h*h)
+}
+console.log(BMI(54,1.75))
